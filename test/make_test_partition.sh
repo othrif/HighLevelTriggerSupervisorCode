70df9285088ae731f787e394b83c8f0dd71dae1d
Reiner Hauser <Reiner.Hauser@cern.ch>
2011-08-22 09:04:16 +0000

    publish HLTSV

--- test/make_test_partition.sh
+++ test/make_test_partition.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+REPOSITORY=/work/msu-pc7/rhauser/tdaq-nightly/installed
 pm_part_l2.py -p test_hltsv
 pm_set.py test_hltsv.data.xml <<EOF
 
@@ -11,14 +12,18 @@ pm_set.py test_hltsv.data.xml <<EOF
   testDCM@Binary.BelongsTo =  Online@SW_Repository
 
   HLTSVConf@DC_ISResourceUpdate
+  HLTSVConf@DC_ISResourceUpdate.name = 'HLTSV'
   HLTSVConf@DC_ISResourceUpdate.delay = 10
   HLTSVConf@DC_ISResourceUpdate.activeOnNodes = [ "L2SV" ]
 
+  DCAppConf-1@DCApplicationConfig.refDC_ISResourceUpdate += [ HLTSVConf@DC_ISResourceUpdate ]
+
   L2SV-1@L2SVApplication.Program = hltsv_main@Binary
   L2SV-1@L2SVApplication.ReceiveMulticast = True
-  DCAppConf-1@DCApplicationConfig.refDC_ISResourceUpdate += [ HLTSVConf@DC_ISResourceUpdate ]
 
   L2PU-1@L2PUTemplateApplication.Program = testDCM@Binary
   L2PU-1@L2PUTemplateApplication.Instances = 8
+
+  test_hltsv@Partition.RepositoryRoot = "${REPOSITORY}"
 
 EOF
