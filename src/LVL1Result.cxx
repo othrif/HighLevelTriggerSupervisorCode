22ece2bf4298ed6bc61db8736f3088ffdd41fd41
Nicoletta Garelli <Nicoletta.Garelli@cern.ch>
2013-04-03 15:51:40 +0000

    dummy src/LVL1Result.cxx to make hltsv compiling

---
+++ src/LVL1Result.cxx
@@ -0,0 +1,18 @@
+
+#include "LVL1Result.h"
+
+#include <iostream>
+
+
+namespace hltsv {
+ 
+
+  LVL1Result::~LVL1Result(){}
+
+
+  uint32_t LVL1Result::l1_id() const {
+    std::cout << "TO DO!" << std::endl;
+    return 999;
+  }
+
+}
