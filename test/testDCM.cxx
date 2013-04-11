06d1b5fd130a5e66b357eeb721e3ac020efbfdb1
Chase Owen Shimmin <chase.shimmin@cern.ch>
2013-04-11 17:23:46 +0000

    Separated DCMActivity declaration and definition

--- test/testDCM.cxx
+++ test/testDCM.cxx
@@ -23,69 +23,94 @@
 #include "DCMMessages.h"
 #include "HLTSVSession.h"
 
-
 class DCMActivity : public daq::rc::Controllable {
 public:
-  DCMActivity(std::string& name)
+  DCMActivity(std::string & name);
+  ~DCMActivity();
+
+  virtual void initialize(std::string & ) {};
+  virtual void configure(std::string & );
+  virtual void unconfigure(std::string & );
+  virtual void connect(std::string & );
+  virtual void stopL2SV(std::string & );
+  virtual void prepareForRun(std::string & );
+
+  void execute();
+
+private:
+  MessageConfiguration m_msgconf;
+  daq::asyncmsg::NameService *m_testns;
+  std::shared_ptr<hltsv::HLTSVSession> m_session;
+
+  boost::asio::io_service m_dcm_io_service;
+  boost::asio::io_service::work m_work;
+
+  bool m_running;
+  ProtectedQueue<dcmessages::LVL1Result*> m_queue;
+  std::vector<boost::thread*>      m_handlers;
+  tbb::atomic<uint32_t>            m_outstanding;
+  uint32_t                         m_cores;
+  
+  uint32_t                         m_l2_processing;
+  uint32_t                         m_l2_accept;
+  uint32_t                         m_event_building;
+  uint32_t                         m_event_filter;
+};
+
+DCMActivity::DCMActivity(std::string &name)
     : daq::rc::Controllable(name),
       m_work(m_dcm_io_service), m_running(false)
-  {
-  }
+{
+}
 
-  ~DCMActivity()
-  {
-  }
+DCMActivity::~DCMActivity()
+{
+}
 
-  virtual void initialize(std::string & )
-  {
-  }
+void DCMActivity::configure(std::string & )
+{
+  Configuration *conf = daq::rc::ConfigurationBridge::instance()->getConfiguration();
 
-  virtual void configure(std::string& )
-  {
-    Configuration *conf = daq::rc::ConfigurationBridge::instance()->getConfiguration();
+  const daq::core::Partition *partition = daq::rc::ConfigurationBridge::instance()->getPartition();
+  IPCPartition part(partition->UID());
+  const daq::df::DFParameters *dfparams = conf->cast<daq::df::DFParameters>(partition->get_DataFlowParameters());
+  m_testns = new daq::asyncmsg::NameService(part, dfparams->get_DefaultDataNetworks());
+  
+}
 
-    const daq::core::Partition *partition = daq::rc::ConfigurationBridge::instance()->getPartition();
-    IPCPartition part(partition->UID());
-    const daq::df::DFParameters *dfparams = conf->cast<daq::df::DFParameters>(partition->get_DataFlowParameters());
-    m_testns = new daq::asyncmsg::NameService(part, dfparams->get_DefaultDataNetworks());
-    
-  }
+void DCMActivity::unconfigure(std::string & )
+{
+  m_msgconf.unconfigure();
+  delete m_testns;
+}
 
-  virtual void unconfigure(std::string &)
-  {
-    m_msgconf.unconfigure();
-    m_ports.clear();
-    delete m_testns;
-  }
+void DCMActivity::connect(std::string &)
+{
+  
+  // Read the HLTSV port using the name Service
+  std::vector<boost::asio::ip::tcp::endpoint> hltsv_eps = m_testns->lookup("HLTSV");
+  ERS_LOG("Found: " << hltsv_eps.size() << " endpoints");
+  ERS_LOG("Found the port: " << hltsv_eps[0].port() );
 
-
-  virtual void connect(std::string& )
-  {
-    
-    // Read the HLTSV port using the name Service
-    std::vector<boost::asio::ip::tcp::endpoint> hltsv_eps = m_testns->lookup("HLTSV");
-    ERS_LOG("Found: " << hltsv_eps.size() << " endpoints");
-    ERS_LOG("Found the port: " << hltsv_eps[0].port() );
-
-    // Start ASIO Client
-    ERS_LOG(" *** Start io_service ***");
-    auto func = [&] () {
-      ERS_LOG(" *** Run io_service ***");
-      m_dcm_io_service.run(); 
-      ERS_LOG(" *** io_service End ***");
-    };
-    boost::thread service_thread(func); 
-    ERS_LOG(" *** installed service thread *** ");
-    // create the session to talk to the HLTSV    
-    m_session = std::make_shared<hltsv::HLTSVSession>(m_dcm_io_service);
-    ERS_LOG(" *** created HLTSVSession *** ");
-    m_session->asyncOpen("HLTSV", hltsv_eps[0]);
-    
-    //    std::vector<uint32_t> l1ids;
-    //l1ids.push_back(3);
-    //std::unique_ptr<const hltsv::RequestMessage> test_msg(new hltsv::RequestMessage(1,l1idss));
-    //m_sessions[0]->onSend(test_msg);
-    // Stop test
+  // Start ASIO Client
+  ERS_LOG(" *** Start io_service ***");
+  auto func = [&] () {
+    ERS_LOG(" *** Run io_service ***");
+    m_dcm_io_service.run(); 
+    ERS_LOG(" *** io_service End ***");
+  };
+  boost::thread service_thread(func); 
+  ERS_LOG(" *** installed service thread *** ");
+  // create the session to talk to the HLTSV    
+  m_session = std::make_shared<hltsv::HLTSVSession>(m_dcm_io_service);
+  ERS_LOG(" *** created HLTSVSession *** ");
+  m_session->asyncOpen("HLTSV", hltsv_eps[0]);
+  
+  //    std::vector<uint32_t> l1ids;
+  //l1ids.push_back(3);
+  //std::unique_ptr<const hltsv::RequestMessage> test_msg(new hltsv::RequestMessage(1,l1idss));
+  //m_sessions[0]->onSend(test_msg);
+  // Stop test
 
 //     MessagePassing::Buffer announce(128);
 
@@ -98,16 +123,24 @@ public:
 //     announce.size(it);
     
 //     m_ports.front()->send(&announce, true);
+}
 
-  }
-  virtual void stopL2SV(std::string &)
-  {
-    m_running = false;
-  }
+void DCMActivity::stopL2SV(std::string & )
+{
+  m_running = false;
+}
 
-  void execute()
-  {
-    ERS_LOG(" *** enter execute() *** ");
+void DCMActivity::prepareForRun(std::string & )
+{
+  ERS_LOG(" *** enter prepareForRun *** ");
+  m_running = true;
+  execute();
+}
+  
+ 
+void DCMActivity::execute()
+{
+  ERS_LOG(" *** enter execute() *** ");
     
 //     using namespace MessagePassing;
 //     using namespace MessageInput;
@@ -151,39 +184,7 @@ public:
         
 //       }
 //     }
-  }
-
-    virtual void prepareForRun(std::string& )
-  {
-    ERS_LOG(" *** enter prepareForRun *** ");
-    m_running = true;
-    execute();
-  }
-  
-  
-
-
-  //*************************
-  
-private:
-  MessageConfiguration   m_msgconf;
-  daq::asyncmsg::NameService *m_testns;
-  std::list<MessagePassing::Port*> m_ports;
-  boost::asio::io_service m_dcm_io_service;
-  boost::asio::io_service::work m_work;
-  std::shared_ptr<hltsv::HLTSVSession> m_session;
-
-  bool m_running;
-  ProtectedQueue<dcmessages::LVL1Result*> m_queue;
-  std::vector<boost::thread*>      m_handlers;
-  tbb::atomic<uint32_t>            m_outstanding;
-  uint32_t                         m_cores;
-  
-  uint32_t                         m_l2_processing;
-  uint32_t                         m_l2_accept;
-  uint32_t                         m_event_building;
-  uint32_t                         m_event_filter;
-};
+}
 
 int main(int argc, char *argv[])
 {
@@ -227,3 +228,4 @@ int main(int argc, char *argv[])
   control.run();
 
 }
+
