803fec2ff226f56bf7c256f3e211ddc0c72172fa
Reiner Hauser <Reiner.Hauser@cern.ch>
2015-10-21 08:48:34 +0000

    Fix the ERS_LOG macro after catch statement.
    It seems to be necessary to put it into curly braces.

--- src/L1RobinnpSource.cxx
+++ src/L1RobinnpSource.cxx
@@ -91,7 +91,9 @@ namespace hltsv {
 	m_input=0;
       } else ERS_LOG(" no input present");
     }
-    catch(...) ERS_LOG(" failed in delete of L1RobinnpSource"<<std::endl);
+    catch(...) {
+       ERS_LOG(" failed in delete of L1RobinnpSource"<<std::endl);
+    }
   }
 
   //______________________________________________________________________________
